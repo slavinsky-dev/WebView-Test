4cefc93a3ac5cc176c57ed38cf57808d2505bf02
slavinsky-dev <62518331+slavinsky-dev@users.noreply.github.com>
2020-04-24 18:20:13 +0300

    Initial commit

---
+++ app/src/main/java/com/slavinskydev/trafficdevilstest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.slavinskydev.trafficdevilstest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+
+    }
+}
